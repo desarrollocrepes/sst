47067d5eaa1f92ab6fe850f9d344bc66fd2bd00f
desarrollocrepes <desarrolloti.bog@crepesywaffles.com>
2026-06-10 11:20:32 -0500

    1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // quitar import si se desinstala tailwind
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ], // quitar tailwindcss() si se desinstala tailwind
 })
